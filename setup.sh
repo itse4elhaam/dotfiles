98ec1d023d93d62c7e1bf69eb2b25797983d8cca
Elhaam <e4elhaam@gmail.com>
2025-01-29 20:42:11 +0500

    feat: wish list for setup.sh

---
+++ setup.sh
@@ -0,0 +1,36 @@
+# if system is not ubuntu, exit out
+echo "Welcome to the installtion script, lets start with updates and then we will be installing some packages along with some conf"
+
+sudo apt update && sudo apt upgrade
+
+
+# installs
+# install nvm, nvm install lts, nvm use lts and nvm default alias lts
+# install nvim - using ppa
+# install tmux - using ppa
+# install lazygit
+# install yazi
+# install stow
+# install python3 and python-is-python3
+# install g (gvm) and set lts as the default alias
+# install redis
+# install fzf
+# install exa
+# install xclip
+# install bun
+# install deno
+
+
+# setup
+# create a user named e4elhaam
+# clone neovim conf
+# install and setup zsh
+# warn the user to install the jet brains mono font and continue on pressing enter
+# install oh my zsh and powerlevel 10k, also run power level 10k conf
+# first mv all dotfiles present in the .dotfiles folder to a bak file and then run stow . in ~/.dotfiles 
+# mkdir ~/coding ~/downloads ~/scripts ~/temp
+
+
+
+
+
