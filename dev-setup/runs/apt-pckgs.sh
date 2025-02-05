9cef69f359951ae2118942e1a316f6534f5ef158
Elhaam <e4elhaam@gmail.com>
2025-02-05 19:36:30 +0500

    refactor: made an array for apt packages

--- dev-setup/runs/apt-pckgs.sh
+++ dev-setup/runs/apt-pckgs.sh
@@ -3,10 +3,28 @@
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
-# open source redis alternative?
-sudo apt install -y \
-  software-properties-common curl git wget unzip build-essential \
-  python3 python-is-python3 \
-  redis fzf exa xclip ripgrep jq gcc make \
-  postgresql postgresql-contrib mysql-server mongodb \
-  stow 
+ packages=(
+  "software-properties-common"
+  "curl"
+  "git"
+  "wget"
+  "unzip"
+  "build-essential"
+  "python3"
+  "python-is-python3"
+  "redis"
+  "fzf"
+  "exa"
+  "xclip"
+  "ripgrep"
+  "jq"
+  "gcc"
+  "make"
+  "postgresql"
+  "postgresql-contrib"
+  "mysql-server"
+  "mongodb"
+  "stow"
+)
+
+sudo apt install -y "${packages[@]}"
