cc4cf9909772e4de6ead405f4fc23294669bb7e0
Elhaam <e4elhaam@gmail.com>
2025-02-05 18:23:30 +0500

    feat: created run.sh for dev-setup script

---
+++ dev-setup/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+filter="$1"
+
+echo "$script_dir -- $filter"
+
+# what does this do
+cd "$script_dir" || exit
+runs_dir="./runs"
+
+if [ ! -d "$runs_dir" ]; then
+  echo "$runs_dir does not exist"
+  exit 1
+fi
+
+scripts="$(find $runs_dir -maxdepth 1 -mindepth 1 -type f)"
+
+for script in $scripts; do
+  # v here inverts it and says if it does not contain it
+  if echo "$script" | grep -qv; then
+    echo "filtering $script"
+    continue 
+  fi
+
+  ./"$script"
+done
