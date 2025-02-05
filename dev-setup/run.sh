b56365285915dd24e5941df7e2e82d8af697203e
Elhaam <e4elhaam@gmail.com>
2025-02-05 19:03:59 +0500

    feat: dry run logic for run.sh

--- dev-setup/run.sh
+++ dev-setup/run.sh
@@ -2,16 +2,48 @@
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-filter="$1"
+filter=""
 
-echo "$script_dir -- $filter"
+dry=0
+
+while [[ $# -gt 1 ]]; do
+  if [[ $1 == "--dry" ]]; then
+    dry=1
+  else 
+    filter="$1"
+  fi
+  # what does this do? this is probably not working
+  shift
+done
+
+
+log(){
+  if [[ $dry == 1 ]]; then
+    echo "[DRY_RUN]: $*"
+    return
+  fi
+
+  echo "$@"
+}
+
+execute(){
+  log "execute $*"
+  if [[ $dry == 1 ]]; then
+    return
+  fi
+
+  # this will just execute it
+  "$@"
+}
+
+log "$script_dir -- $filter"
 
 # what does this do
 cd "$script_dir" || exit
 runs_dir="./runs"
 
 if [ ! -d "$runs_dir" ]; then
-  echo "$runs_dir does not exist"
+  log "$runs_dir does not exist"
   exit 1
 fi
 
@@ -20,9 +52,9 @@ scripts="$(find $runs_dir -maxdepth 1 -mindepth 1 -type f)"
 for script in $scripts; do
   # v here inverts it and says if it does not contain it
   if echo "$script" | grep -qv "$filter"; then
-    echo "filtering $script"
+    log "filtering $script"
     continue 
   fi
 
-  ./"$script"
+  execute ./"$script"
 done
